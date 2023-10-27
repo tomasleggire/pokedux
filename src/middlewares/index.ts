36c14491815705140b23cfa9d08fdda2db48691c
Tomás <tomasleggire@hotmail.com>
2023-10-26 21:10:23 -0300

    Arreglos en type de poke

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,19 +1,30 @@
+import { PokemonType } from "../actions/types";
+
 export const logger = (store: any) => (next: any) => (action: any) => {
-    console.log(action);
-    next(action);
-}
+  console.log(action);
+  console.log(store);
+  next(action);
+};
 
 export const featuring = (store: any) => (next: any) => (actionInfo: any) => {
-    const featured = [{ name: 'eddie' }, ...actionInfo.payload];
-    const updatedActionInfo = {
-      ...actionInfo,
-      payload: [...featured],
-    };
+  console.log(store);
+  const featured = [{ name: "eddie" }, ...actionInfo.payload];
+  const updatedActionInfo = {
+    ...actionInfo,
+    payload: [...featured],
+  };
+  next(updatedActionInfo);
+};
+
+export const pokeUpperCaseName =
+  (store: any) => (next: any) => (actionInfo: any) => {
+    console.log(store);
+    const upperCase = [
+      ...actionInfo.payload.map((poke: PokemonType) => ({
+        ...poke,
+        name: poke.name.toUpperCase(),
+      })),
+    ];
+    const updatedActionInfo = { ...actionInfo, payload: [...upperCase] };
     next(updatedActionInfo);
   };
-
-export const pokeUpperCaseName = (store: any) => (next: any) => (actionInfo: any) => {
-    const upperCase = [...actionInfo.payload.map(poke => ({...poke, name: poke.name.toUpperCase()}))]
-    const updatedActionInfo = {...actionInfo, payload: [...upperCase]}
-    next(updatedActionInfo);
-}
